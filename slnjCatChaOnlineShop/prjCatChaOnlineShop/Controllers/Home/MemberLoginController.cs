63e39e1a47ca56b91d7544f9ff4e6dc508130445
April <smilehsiang@gmail.com>
2023-08-16 10:18:22 +0800

    修正版面問題

---
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/MemberLoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjCatChaOnlineShop.Controllers.Home
+{
+    public class MemberLoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterMember() 
+        {
+            return View();
+        }
+    }
+}
