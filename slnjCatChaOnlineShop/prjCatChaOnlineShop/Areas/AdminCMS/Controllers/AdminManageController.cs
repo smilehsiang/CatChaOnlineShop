2c819a0bc298a647e52f756a7ca5b5d517de1e5e
MANDY LIANG <liang930517@yahoo.com.tw>
2023-08-17 17:16:16 +0800

    測試連接資料庫

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/AdminManageController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/AdminManageController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjCatChaOnlineShop.Models;
+using System.Security.Policy;
 
 namespace prjCatChaOnlineShop.Areas.AdminCMS.Controllers
 
@@ -6,10 +8,15 @@ namespace prjCatChaOnlineShop.Areas.AdminCMS.Controllers
     [Area("AdminCMS")]
     public class AdminManageController : Controller
     {
-        
+        private readonly cachaContext _context; //新的注入方法
+        public AdminManageController(cachaContext context)
+        {
+            _context = context;
+        }
         public IActionResult AdminManage()
         {
-            return View();
+            var list = _context.ShopMemberInfo;
+            return Json(list);
         }
     }
 }
