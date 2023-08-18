45303d3f52cba8707784ce32d3cc49130438bf45
April <smilehsiang@gmail.com>
2023-08-18 13:33:06 +0800

    Login功能v1.0陽春版

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/MemberLoginController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/MemberLoginController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjCatChaOnlineShop.Models.CDictionary;
+using prjCatChaOnlineShop.Models.ViewModels;
+using prjCatChaOnlineShop.Models;
+using System.Text.Json;
 
 namespace prjCatChaOnlineShop.Controllers.Home
 {
@@ -6,6 +10,20 @@ namespace prjCatChaOnlineShop.Controllers.Home
     {
         public IActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(CLoginModel vm)
+        {
+            
+            ShopMemberInfo user = (new cachaContext()).ShopMemberInfo.FirstOrDefault(
+                t => t.Email.Equals(vm.txtEmail) && t.Password.Equals(vm.txtPassword));
+            if (user != null && user.Password.Equals(vm.txtPassword))
+            {
+                string Json = JsonSerializer.Serialize(user);
+                HttpContext.Session.SetString(CDictionary.SK_LOINGED_USER, Json);
+                return RedirectToAction("Index", "Index");
+            }
             return View();
         }
 
