81d9a68c76025a795a44bd850393c9b05e50dbfe
DESKTOP-IGIUBK4\yaolin <peter19971115@gmail.com>
2023-08-21 03:43:55 +0800

    ·轉蛋功能-單抽十抽修復 ·轉蛋功能-資料庫修改道具數量及機率時不影響抽卡功能 ·修正圖片顯示過大 ·新增部分道具圖片 ---------------------------------------------------- 待修正 ·抽獎道具名稱尚未加入 ·排版尚未調整 ·登入使用者扣款抽獎

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/wwwroot/js/game/Summon.js
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/wwwroot/js/game/Summon.js
@@ -43,80 +43,296 @@ async function fetchData() {
                 scaledProbability
             });
         });
-
         return processedData; // 返回處理後的資料
     } catch (error) {
         console.error('錯誤:', error);
     }
 }
 
-    RubyTenDrows.addEventListener('click', async function () {
+RubyTenDrows.addEventListener('click', async function () {
     try {
         const gachaData = await fetchData(); // 取得轉蛋資料
-        const numDraws = 1000; // 設定抽獎次數，這裡設定為 1 次
+        const numDraws = 10;
+        const drawResults = [];
+        const allImages = [];
+
         for (let i = 0; i < numDraws; i++) {
-            const randomIndex = Math.floor(Math.random() * gachaData.scaledProbability);
+            const randomValue = Math.floor(Math.random() * 100) + 1; // 生成1到100之間的隨機數
+
+            // 初始化索引和臨時總數
+            let randomIndex = -1;
+            let tempSum = 0;
+
+            // 遍歷資料以找到對應的索引
+            for (let j = 0; j < gachaData.length; j++) {
+                tempSum += gachaData[j].scaledProbability; // 累積縮放後的機率
+
+                if (randomValue <= tempSum) {
+                    randomIndex = j;
+                    break;
+                }
+            }
             const drawnItem = gachaData[randomIndex];
-            console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName}`);
+            if (drawnItem && drawnItem.productName) {
+                // 將抽獎結果儲存在陣列中
+                drawResults.push(drawnItem);
+                allImages.push(drawnItem.productImage);
+                console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName},${drawnItem.scaledProbability},${drawnItem.productImage}`);
+                display = `<div class="item-container" style="margin-left:320px;">${drawnItem.productImage}</div>`;
+            } else {
+                i--; // 減少i以重新執行本次抽獎
+            }
+        }
+
+        // 計算最高等級的抽獎結果
+        let maxScaledProbability = 100; // 初始設為100，確保每個機率都比它大
+        let maxResult = null;
+        for (const result of drawResults) {
+            if (result.scaledProbability < maxScaledProbability) {
+                maxScaledProbability = result.scaledProbability;
+                maxResult = result;
+            }
+        }
+
+        // 顯示最高等級的動畫和結果，並傳遞所有物品的圖片
+        if (maxResult) {
+            showGachaResult(maxResult.scaledProbability, allImages);
+            console.log(maxResult.productName);
         }
     } catch (error) {
         console.error('轉蛋時發生錯誤:', error);
-        }
-    }); 
-    CatPointTenDrows.addEventListener('click', async function () {
-        try {
-            const gachaData = await fetchData(); // 取得轉蛋資料
-            const numDraws = 10; // 設定抽獎次數
+    }
+});
+CatPointTenDrows.addEventListener('click', async function () {
+    try {
+        const gachaData = await fetchData(); // 取得轉蛋資料
+        const numDraws = 10;
+        const drawResults = [];
+        const allImages = [];
 
-            for (let i = 0; i < numDraws; i++) {
-                const randomValue = Math.floor(Math.random() * 100) + 1; // 隨機整數 1 到 100
-                let drawnItem = null;
-                let cumulativeRangeEnd = 0;
+        for (let i = 0; i < numDraws; i++) {
+            const randomValue = Math.floor(Math.random() * 100) + 1; // 生成1到100之間的隨機數
 
-                // 在區間累加過程中找到對應的項目
-                for (const item of gachaData) {
-                    cumulativeRangeEnd += item.rangeEnd;
-                    if (randomValue <= cumulativeRangeEnd) {
-                        drawnItem = item;
-                        break;
-                    }
-                }
+            // 初始化索引和臨時總數
+            let randomIndex = -1;
+            let tempSum = 0;
 
-                if (drawnItem) {
-                    console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName}`);
+            // 遍歷資料以找到對應的索引
+            for (let j = 0; j < gachaData.length; j++) {
+                tempSum += gachaData[j].scaledProbability; // 累積縮放後的機率
+
+                if (randomValue <= tempSum) {
+                    randomIndex = j;
+                    break;
                 }
             }
-        } catch (error) {
-            console.error('轉蛋時發生錯誤:', error);
+            const drawnItem = gachaData[randomIndex];
+            if (drawnItem && drawnItem.productName) {
+                // 將抽獎結果儲存在陣列中
+                drawResults.push(drawnItem);
+                allImages.push(drawnItem.productImage);
+                console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName},${drawnItem.scaledProbability},${drawnItem.productImage}`);
+                display = `<div class="item-container" style="margin-left:320px;">${drawnItem.productImage}</div>`;
+            } else {
+                i--; // 減少i以重新執行本次抽獎
+            }
         }
+
+        // 計算最高等級的抽獎結果
+        let maxScaledProbability = 100; // 初始設為100，確保每個機率都比它大
+        let maxResult = null;
+        for (const result of drawResults) {
+            if (result.scaledProbability < maxScaledProbability) {
+                maxScaledProbability = result.scaledProbability;
+                maxResult = result;
+            }
+        }
+
+        // 顯示最高等級的動畫和結果，並傳遞所有物品的圖片
+        if (maxResult) {
+            showGachaResult(maxResult.scaledProbability, allImages);
+            console.log(maxResult.productName);
+        }
+    } catch (error) {
+        console.error('轉蛋時發生錯誤:', error);
+    }
+});
+
+CatPointSingleDrow.addEventListener('click', async function () {
+    try {
+        const gachaData = await fetchData(); // 取得轉蛋資料
+        const numDraws = 1;
+        const drawResults = [];
+        const allImages = [];
+
+        for (let i = 0; i < numDraws; i++) {
+            const randomValue = Math.floor(Math.random() * 100) + 1; // 生成1到100之間的隨機數
+
+            // 初始化索引和臨時總數
+            let randomIndex = -1;
+            let tempSum = 0;
+
+            // 遍歷資料以找到對應的索引
+            for (let j = 0; j < gachaData.length; j++) {
+                tempSum += gachaData[j].scaledProbability; // 累積縮放後的機率
+
+                if (randomValue <= tempSum) {
+                    randomIndex = j;
+                    break;
+                }
+            }
+            const drawnItem = gachaData[randomIndex];
+            if (drawnItem && drawnItem.productName) {
+                // 將抽獎結果儲存在陣列中
+                drawResults.push(drawnItem);
+                allImages.push(drawnItem.productImage);
+                console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName},${drawnItem.scaledProbability},${drawnItem.productImage}`);
+                display = `<div class="item-container" style="margin-left:320px;">${drawnItem.productImage}</div>`;
+            } else {
+                i--; // 減少i以重新執行本次抽獎
+            }
+        }
+
+        // 計算最高等級的抽獎結果
+        let maxScaledProbability = 100; // 初始設為100，確保每個機率都比它大
+        let maxResult = null;
+        for (const result of drawResults) {
+            if (result.scaledProbability < maxScaledProbability) {
+                maxScaledProbability = result.scaledProbability;
+                maxResult = result;
+            }
+        }
+
+        // 顯示最高等級的動畫和結果，並傳遞所有物品的圖片
+        if (maxResult) {
+            showGachaResult(maxResult.scaledProbability, allImages);
+            console.log(maxResult.productName);
+        }
+    } catch (error) {
+        console.error('轉蛋時發生錯誤:', error);
+    }
+});
+RubySingleDrow.addEventListener('click', async function () {
+    try {
+        const gachaData = await fetchData(); // 取得轉蛋資料
+        const numDraws = 1;
+        const drawResults = [];
+        const allImages = [];
+
+        for (let i = 0; i < numDraws; i++) {
+            const randomValue = Math.floor(Math.random() * 100) + 1; // 生成1到100之間的隨機數
+
+            // 初始化索引和臨時總數
+            let randomIndex = -1;
+            let tempSum = 0;
+
+            // 遍歷資料以找到對應的索引
+            for (let j = 0; j < gachaData.length; j++) {
+                tempSum += gachaData[j].scaledProbability; // 累積縮放後的機率
+
+                if (randomValue <= tempSum) {
+                    randomIndex = j;
+                    break;
+                }
+            }
+            const drawnItem = gachaData[randomIndex];
+            if (drawnItem && drawnItem.productName) {
+                // 將抽獎結果儲存在陣列中
+                drawResults.push(drawnItem);
+                allImages.push(drawnItem.productImage);
+                console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName},${drawnItem.scaledProbability},${drawnItem.productImage}`);
+            } else {
+                i--; // 減少i以重新執行本次抽獎
+            }
+        }
+
+        // 計算最高等級的抽獎結果
+        let maxScaledProbability = 100; // 初始設為100，確保每個機率都比它大
+        let maxResult = null;
+        for (const result of drawResults) {
+            if (result.scaledProbability < maxScaledProbability) {
+                maxScaledProbability = result.scaledProbability;
+                maxResult = result;
+            }
+        }
+
+        // 顯示最高等級的動畫和結果，並傳遞所有物品的圖片
+        if (maxResult) {
+            showGachaResult(maxResult.scaledProbability, allImages);
+            console.log(maxResult.productName);
+        }
+    } catch (error) {
+        console.error('轉蛋時發生錯誤:', error);
+    }
+});
+
+function showGachaResult(scaledProbability, allImages) {
+    // 將四個button關掉顯示
+    summonbuttons.style.display = 'none';
+    // 用於儲存要顯示的動畫等級
+    let animationLevel = '';
+
+    // 檢查是否有SS等級的獎項，如果有則設置動畫等級為'SS'
+    if (scaledProbability <= 5) {
+        animationLevel = 'SS';
+    } else if (scaledProbability <= 10) {
+        animationLevel = 'S';
+    } else {
+        animationLevel = 'A';
+    }
+
+    // 根據動畫等級顯示相應的動畫
+    if (animationLevel === 'SS') {
+        animationContainer.querySelector('.SS').style.display = 'inline';
+    } else if (animationLevel === 'S') {
+        animationContainer.querySelector('.S').style.display = 'inline';
+    } else {
+        animationContainer.querySelector('.A').style.display = 'inline';
+    }
+
+    // 顯示所有物品的圖片
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('item-container');
+    allImages.forEach(imageSrc => {
+        const itemImage = document.createElement('img');
+        itemImage.src = imageSrc;
+        imageContainer.appendChild(itemImage);
     });
 
-    RubySingleDrow.addEventListener('click', async function () {
-            try {
-                const gachaData = await fetchData(); // 取得轉蛋資料
-                const numDraws = 1; // 設定抽獎次數，這裡設定為 1 次
-                for (let i = 0; i < numDraws; i++) {
-                    const randomIndex = Math.floor(Math.random() * gachaData.length);
-                    const drawnItem = gachaData[randomIndex];
-                    console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName}`);
-                }
-            } catch (error) {
-                console.error('轉蛋時發生錯誤:', error);
-            }
+    // 將圖片容器添加到結果中
+    result.appendChild(imageContainer);
+
+    setTimeout(() => {
+        animationImages.forEach(image => {
+            image.style.display = 'none';
         });
-    CatPointSingleDrow.addEventListener('click', async function () {
-            try {
-                const gachaData = await fetchData(); // 取得轉蛋資料
-                const numDraws = 1; // 設定抽獎次數，這裡設定為 1 次
-                for (let i = 0; i < numDraws; i++) {
-                    const randomIndex = Math.floor(Math.random() * gachaData.length);
-                    const drawnItem = gachaData[randomIndex];
-                    console.log(`第 ${i + 1} 次轉蛋：你獲得了 ${drawnItem.productName}`);
-                }
-            } catch (error) {
-                console.error('轉蛋時發生錯誤:', error);
-            }
+
+        // 插入抽獎物品的圖片到抽獎格子中
+        const resultContainer = document.querySelector('.result-container');
+        resultContainer.style.display = 'display';
+
+        // 生成確認按鈕
+        const confirmButton = document.createElement('button');
+        confirmButton.textContent = '確認';
+        confirmButton.style.fontSize = '20px';
+        confirmButton.style.width = '70px';
+        confirmButton.style.height = '50px';
+
+        confirmButton.addEventListener('click', () => {
+            // 點擊確認按鈕後，關閉獎項顯示，並顯示四個button
+            result.innerHTML = '結果:';
+            result.style.display = 'none';
+            summonbuttons.style.display = 'block';
         });
+
+        // 將確認按鈕添加到獎項後面
+        result.appendChild(confirmButton);
+        result.style.display = 'grid';
+    }, 5000); // 5000毫秒等於5秒
+}
+
+
+
     // Button 4 點擊事件處理
     //button4.addEventListener('click', () => {
     //    // 顯示轉蛋頁面
@@ -240,74 +456,74 @@ async function fetchData() {
 
 
     // 可重複使用的開關動畫&開關按鍵，專門用來控制顯示的部分
-    function showGachaResult(display, tenorsingle) {
-        // 將四個button關掉顯示
-        summonbuttons.style.display = 'none';
-        // 檢查是否有SS等級的獎項，如果有則顯示SS動畫
-        const hasSSItem = display.includes('SS');
-        if (hasSSItem) {
-            animationContainer.querySelector('.SS').style.display = 'inline';
-        } else {
-            // 檢查是否有S等級的獎項，如果有則顯示S動畫
-            const hasSItem = display.includes('S');
-            if (hasSItem) {
-                animationContainer.querySelector('.S').style.display = 'inline';
-            } else {
-                // 沒有SS和S等級的獎項，顯示A動畫
-                animationContainer.querySelector('.A').style.display = 'inline';
-            }
-        }
-        setTimeout(() => {
-            animationImages.forEach(image => {
-                image.style.display = 'none';
-            });
+    //function showGachaResult(display/*, tenorsingle*/) {
+    //    // 將四個button關掉顯示
+    //    summonbuttons.style.display = 'none';
+    //    // 檢查是否有SS等級的獎項，如果有則顯示SS動畫
+    //    const hasSSItem = display.includes('SS');
+    //    if (hasSSItem) {
+    //        animationContainer.querySelector('.SS').style.display = 'inline';
+    //    } else {
+    //        // 檢查是否有S等級的獎項，如果有則顯示S動畫
+    //        const hasSItem = display.includes('S');
+    //        if (hasSItem) {
+    //            animationContainer.querySelector('.S').style.display = 'inline';
+    //        } else {
+    //            // 沒有SS和S等級的獎項，顯示A動畫
+    //            animationContainer.querySelector('.A').style.display = 'inline';
+    //        }
+    //    }
+    //    setTimeout(() => {
+    //        animationImages.forEach(image => {
+    //            image.style.display = 'none';
+    //        });
 
-            // 顯示獎項
-            result.innerHTML = display;
+    //        // 顯示獎項
+    //        result.innerHTML = display;
 
-            // 生成確認按鈕
-            const confirmButton = document.createElement('button');
-            x = tenorsingle;
-            //判斷10抽或1抽 調整版型
+    //        // 生成確認按鈕
+    //        const confirmButton = document.createElement('button');
+    //        //x = tenorsingle;
+    //        ////判斷10抽或1抽 調整版型
 
-            if (x == 0 | x == 0) {
-                confirmButton.textContent = '確認'
-                confirmButton.style.fontSize = '20px'; // 設定文字大小為20px
-                confirmButton.style.width = '70px'; // 設定寬度為70px
-                confirmButton.style.height = '50px'; // 設定高度為50px
-                confirmButton.style.marginLeft = '320px';
-                confirmButton.style.marginBottom = '250px';
-            } else if (x == 1 | x == 1) {
-                confirmButton.textContent = '確認';
-                confirmButton.style.fontSize = '20px'; // 設定文字大小為20px
-                confirmButton.style.width = '70px'; // 設定寬度為70px
-                confirmButton.style.height = '50px'; // 設定高度為50px
-                confirmButton.style.marginTop = '200px'; // 
-                confirmButton.style.marginLeft = '160px'; //
-            }
-
-
-            // 如果需要共用的 margin-left 設定，可以將這行代碼放在 if-else 外面
-            // confirmButton
+    //        //if (x == 0 | x == 0) {
+    //        //    confirmButton.textContent = '確認'
+    //        //    confirmButton.style.fontSize = '20px'; // 設定文字大小為20px
+    //        //    confirmButton.style.width = '70px'; // 設定寬度為70px
+    //        //    confirmButton.style.height = '50px'; // 設定高度為50px
+    //        //    confirmButton.style.marginLeft = '320px';
+    //        //    confirmButton.style.marginBottom = '250px';
+    //        //} else if (x == 1 | x == 1) {
+    //        //    confirmButton.textContent = '確認';
+    //        //    confirmButton.style.fontSize = '20px'; // 設定文字大小為20px
+    //        //    confirmButton.style.width = '70px'; // 設定寬度為70px
+    //        //    confirmButton.style.height = '50px'; // 設定高度為50px
+    //        //    confirmButton.style.marginTop = '200px'; // 
+    //        //    confirmButton.style.marginLeft = '160px'; //
+    //        //}
 
 
-            /*     confirmButton.style.marginTop = '50px';*/
-            // 將確認按鈕添加到確認容器內
+    //        // 如果需要共用的 margin-left 設定，可以將這行代碼放在 if-else 外面
+    //        // confirmButton
 
 
-
-            confirmButton.addEventListener('click', () => {
-                // 點擊確認按鈕後，關閉獎項顯示
-                // 將四個button打開
-                result.innerHTML = '結果:';
-                result.style.display = 'none';
-                summonbuttons.style.display = 'block';
-            });
+    //        /*     confirmButton.style.marginTop = '50px';*/
+    //        // 將確認按鈕添加到確認容器內
 
 
 
-            // 將確認按鈕添加到獎項後面
-            result.appendChild(confirmButton);
-            result.style.display = 'grid';
-        }, 5000); // 5000毫秒等於5秒
-    }
+    //        confirmButton.addEventListener('click', () => {
+    //            // 點擊確認按鈕後，關閉獎項顯示
+    //            // 將四個button打開
+    //            result.innerHTML = '結果:';
+    //            result.style.display = 'none';
+    //            summonbuttons.style.display = 'block';
+    //        });
+
+
+
+    //        // 將確認按鈕添加到獎項後面
+    //        result.appendChild(confirmButton);
+    //        result.style.display = 'grid';
+    //    }, 5000); // 5000毫秒等於5秒
+    //}
