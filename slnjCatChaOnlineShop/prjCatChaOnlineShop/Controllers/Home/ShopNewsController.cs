46c6a4555847dde196e86f2bc3fe9b269aa0b136
pony0278 <steam2744@gmail.com>
2023-08-22 00:25:29 +0800

    no message

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/ShopNewsController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Controllers/Home/ShopNewsController.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjCatChaOnlineShop.Models;
+using prjCatChaOnlineShop.Models.ViewModels;
 
 namespace prjCatChaOnlineShop.Controllers.Home
 {
     public class ShopNewsController : Controller
     {
+        private readonly cachaContext _context ;
+        
+        public ShopNewsController(cachaContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult NewsContent()
         { 
             return View();
         }
         public IActionResult News()
         {
-            return View();
+            DateTime currentTime = DateTime.Now;
+
+            var NewsModel = new CNewsModel
+            {
+                NewsType = _context.AnnouncementTypeData.ToList(),
+                NewsContent = _context.GameShopAnnouncement
+                                    .Where(p => p.PublishTime <= currentTime && p.PublishEndTime >= currentTime)
+                                    .ToList()
+            };
+
+            return View(NewsModel);
         }
     }
 }
