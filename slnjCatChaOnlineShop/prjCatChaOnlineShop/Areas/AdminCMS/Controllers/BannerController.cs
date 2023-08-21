9447df91afb3f0b04e4e638f53f2c1d483830b12
mina0704 <pineapple0704@gmail.com>
2023-08-21 12:48:42 +0800

    新增BannerWrap

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/BannerController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/BannerController.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using prjCatChaOnlineShop.Models;
+using prjCatChaOnlineShop.Services.Function;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 
 namespace prjCatChaOnlineShop.Controllers.CMS
 {
     [Area("AdminCMS")]
     public class BannerController : Controller
     {
-        
+        private readonly ImageService _imageService;
+        private readonly cachaContext _cachaContext;
+
+
+        public BannerController(ImageService imageService, cachaContext cachaContext)
+        {
+            _imageService = imageService;
+            _cachaContext = cachaContext;
+        }
         public IActionResult Banner()
         {
             return View();
