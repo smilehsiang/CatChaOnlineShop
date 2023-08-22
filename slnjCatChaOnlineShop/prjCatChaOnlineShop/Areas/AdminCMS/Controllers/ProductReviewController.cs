8d45e7905f76b2147c58ff930734d095c18a9a48
pony0278 <steam2744@gmail.com>
2023-08-22 23:14:45 +0800

    no message

--- slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/ProductReviewController.cs
+++ slnjCatChaOnlineShop/prjCatChaOnlineShop/Areas/AdminCMS/Controllers/ProductReviewController.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjCatChaOnlineShop.Models;
 
 namespace prjCatChaOnlineShop.Controllers.CMS
 {
     [Area("AdminCMS")]
     public class ProductReviewController : Controller
     {
+        private readonly cachaContext? _context;
        
+        public ProductReviewController(cachaContext context) 
+        {
+            _context = context;
+        }
+
         public IActionResult ProductReview()
         {
             return View();
+        }
+        public IActionResult tableData()
+        {
+            var rawData = _context.ShopProductReviewTable.ToList();
+            var tableDatas = rawData.Select(x =>
+            {
+                DateTime parsedDateTime;
+                string formattedDateTime = DateTime.TryParse(x.ReviewTime, out parsedDateTime) ? parsedDateTime.ToString("yyyy-MM-dd HH:mm") : "未設定時間";
+                                                            
+            })
         }
     }
 }
